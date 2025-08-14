094384b9feabb26af4708be6eda1bf029f8cd9c1
weiiii7777 <coco30933@gmail.com>
2025-08-14 15:03:16 +0800

    0811

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import App from './pages/App0806-2'
+import App from './pages/App0811-1'
 
 createRoot(document.getElementById('root')).render(
   // 嚴謹模式
---
+++ react-project/src/pages/App0811-1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App0811-1</div>
+  )
+}
+
+export default App
